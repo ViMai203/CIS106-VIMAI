290b51cfb8fa8d766b8529eb053be7b523d80460
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-14 15:03:40 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,14 +1,11 @@
+def calcGrossPay(hours,rate):
+week=hours*rate
+year=52*week
+month=year/12
+print("Weekly gross pay : "+str(round(week,2)))
+print("Monthly gross pay : "+str(round(month,2)))
+print("Annual gross pay : "+str(round(year,2)))
 
-# This program used to asking user age.
-print("Enter age: ")
-age = int(input())
-age = age
-month = age * 12
-day = age * 12 * 365 + float(age) / 4
-hour = (age * 12 * 365 + float(age) / 4) * 24
-second = (age * 12 * 365 + float(age) / 4) * 24 * 60 * 60
-print("Person is " + str(age) + " years old")
-print("Person is " + str(month) + " months old")
-print("Person is " + str(day) + " days old")
-print("Person is " + str(hour) + " hours old")
-print("Person is " + str(second) + " seconds old")
+hours=int(input("Enter hours worked per week : "))
+rate=float(input("Enter rate per hour : "))
+calcGrossPay(hours,rate)
