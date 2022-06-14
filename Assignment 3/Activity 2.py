f40dc6a051556fef257db25a088492644018f260
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-14 06:28:32 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,12 +1,14 @@
 
-#Asking user age
-age=float(input("Enter age:"))
-print("Person is ",age,"years old.")
-#calculate print age in Months
-print("Person is ",age*12,"months old")
-#calculate print age in days
-print("Person is ",age*12*365+age/4,"days old")
-#calculate print age in hours
-print("Person is ",(age*12*365+age/4)*24,"hours old")
-#calculate print age in seconds
-print("Person is ",(age*12*365+age/4)*24*60*60,"seconds old")
+# This program used to asking user age.
+print("Enter age: ")
+age = int(input())
+age = age
+month = age * 12
+day = age * 12 * 365 + float(age) / 4
+hour = (age * 12 * 365 + float(age) / 4) * 24
+second = (age * 12 * 365 + float(age) / 4) * 24 * 60 * 60
+print("Person is " + str(age) + " years old")
+print("Person is " + str(month) + " months old")
+print("Person is " + str(day) + " days old")
+print("Person is " + str(hour) + " hours old")
+print("Person is " + str(second) + " seconds old")
