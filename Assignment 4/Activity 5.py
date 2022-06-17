cdfcbb65ebc3a50b3782c2a297af265cf5d86df6
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-17 06:09:18 -0500

    Update Activity 5.py

--- Assignment 4/Activity 5.py
+++ Assignment 4/Activity 5.py
@@ -3,12 +3,11 @@
 length = float(input('Enter length of the room: '))
 breadth = float(input('Enter breadth of the room: '))
 
-#convert feet to yards
-lengthInYards = length / 3
-breadthYnYyards = breadth / 3
+#convert feet into yards
+L = length / 3
+W = breadth / 3
 
-#multiply length and breadth
-floorAarea = lengthInYards * breadthInYards
+#multiply length and breadth 
+floorarea = L * W
 
-
-print(f'The area of the floor covering required is: {round(floorArea, 2)} sq. yards')
+print(f'The area of the floor covering required is: {round(floorarea, 2)} sq. yards')
