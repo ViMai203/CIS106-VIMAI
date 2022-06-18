0ba11fc150559c17a212daf3381d144aac3fde77
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-18 04:23:25 -0500

    Create Activity 3(2).py

---
+++ Assignment 4/Activity 3(2).py
@@ -0,0 +1,9 @@
+
+#A program calculate and display the distance from miles into kilometers, meters, and centimeters.
+miles = int(input("Input miles: "))
+kilometers = miles * 1.609344
+meters = miles * 1.609
+centimeters = miles * 160.934
+print("The distance in kilometers is " +str(kilometers) +" in")
+print("The distance in meters is " +str(meters) +" yd")
+print("The distance in centimeters is " +str(centimeters) +" ft")
