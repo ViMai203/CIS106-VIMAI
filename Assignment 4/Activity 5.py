178cf229bd9c8648aacd60f53e14640982d71c1e
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-18 04:00:07 -0500

    Update Activity 5.py

--- Assignment 4/Activity 5.py
+++ Assignment 4/Activity 5.py
@@ -1,13 +1,7 @@
 
 # A program that calculates the area of a room to determine the amount of floor covering required
-length = float(input('Enter length of the room: '))
-breadth = float(input('Enter breadth of the room: '))
-
-#convert feet into yards
-L = length / 3
-W = breadth / 3
-
-#multiply length and breadth 
-floorarea = L * W
-
-print(f'The area of the floor covering required is: {round(floorarea, 2)} sq. yards')
+length=float(input("Enter the lenght of the room: "))
+width=float(input("Enter the width of the room: "))
+area = length * width
+totalft= area / 9
+print("Area of the room is "+ str(totalft) +" square yards")
