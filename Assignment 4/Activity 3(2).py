4f44a7a80331e9b10316ad8efc54db3ea77917e7
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-18 04:23:56 -0500

    Update Activity 3(2).py

--- Assignment 4/Activity 3(2).py
+++ Assignment 4/Activity 3(2).py
@@ -4,6 +4,6 @@ miles = int(input("Input miles: "))
 kilometers = miles * 1.609344
 meters = miles * 1.609
 centimeters = miles * 160.934
-print("The distance in kilometers is " +str(kilometers) +" in")
-print("The distance in meters is " +str(meters) +" yd")
-print("The distance in centimeters is " +str(centimeters) +" ft")
+print("The distance in kilometers is " +str(kilometers) +" km")
+print("The distance in meters is " +str(meters) +" m")
+print("The distance in centimeters is " +str(centimeters) +" cm")
