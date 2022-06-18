e158bdb880405b8ad8d075a4a7232fd7bbd27405
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-18 16:54:21 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -4,7 +4,7 @@ age=float(input("Enter age:"))
 month = age * 12
 day = (age * 365) 
 hour = (age * 365 + float(age) / 4) * 24
-second = (age * 365 + float(age) / 4) * 24 * 60 * 60
+second = age * 365 * 24 * 60 
 print("Person is " + str(age) + " years old")
 print("Person is " + str(month) + " months old")
 print("Person is " + str(day) + " days old")
