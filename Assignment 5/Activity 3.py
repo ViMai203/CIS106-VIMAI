5b4432078828ef5ecd5ddeec57afcfbaa7107d43
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-06-21 23:44:35 -0500

    Create Activity 3.py

---
+++ Assignment 5/Activity 3.py
@@ -0,0 +1,40 @@
+# A program that asks the user for a distance in miles, 
+# then converts it to a distance in meters, feet, and inches, 
+# and displays the answear.
+#
+# References:
+# https://en.wikiversity.org/wiki/Programming_Fundamentals/Functions
+# https://www.mathsisfun.com/measure/us-standard-length.html
+def get_miles():
+    print ("Input distance in miles: ")
+    miles = float(input())
+    return miles
+def cal_inches(miles):
+    inches = miles * 63.360
+    return inches
+def cal_yards(miles):
+    yards = miles * 1.760
+    return yards
+def cal_feet(miles):
+    feet = miles * 5.280
+    return feet
+def display_result(miles, inches):
+    print("The distance in inches is " +str(inches) +" in")
+    
+def display_result(miles, yards):
+    print("The distance in yards is " +str(yards) +" yd")
+    
+def display_result(miles, feet):
+    print("The distance in feet is " +str(feet) +" ft")
+    
+def main() :
+    miles = get_miles()
+    inches = cal_inches(miles)
+    yards = cal_yards(miles)
+    feet = cal_feet(miles)
+    display_result(miles, inches)
+    display_result(miles, yards)
+    display_result(miles, feet)
+    
+main()
+   
