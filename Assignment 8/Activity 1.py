0967e036037c9c18fefd9fb69a2316a8dfa9791d
ViMai203 <107064826+ViMai203@users.noreply.github.com>
2022-07-07 22:21:47 -0500

    Update Activity 1.py

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -3,12 +3,4 @@
 #
 # References:
 # https://en.wikiversity.org/wiki/Programming_Fundamentals/Loops
-print("Enter the value: ")
-value = int(input())
-print("Enter the number of expressions: ")
-expressions = int(input())
-number = 1
-while number <= expressions:
-    result = value * number
-    print(str(value) + " * " + str(number) + " = " + str(result))
-    number = number + 1
+
